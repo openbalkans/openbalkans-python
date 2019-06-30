0290b1b62d18d071a96d7531e91d33d827f3907d
Barak Avrahami <19807724+andecy64@users.noreply.github.com>
2019-06-30 19:59:55 +0300

    Added entities (user, post), scrypt for warpwallet

---
+++ openbalkans/entities.py
@@ -0,0 +1,20 @@
+from .common import *
+
+
+class User:
+    
+    def __init__(self, designation=None):
+        if designation:
+            pass
+    
+    @classmethod
+    def from_warp_wallet(self, passphrase, salt):
+        pass
+
+    @staticmethod
+    def _get_key_by_designation(self, designation):
+        pass
+
+
+class Post:
+    pass
